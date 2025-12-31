df22761b3621e69e0761ffcb86e57fba2746fe4c
Beni80amz <benisolehudin@madrasah.kemenag.go.id>
2026-01-01 02:50:40 +0700

    Add admin bypass for maintenance mode

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Exclude admin routes from maintenance mode
+        // This allows admins to access the panel even when the site is down
+        $middleware->preventRequestsDuringMaintenance(except: [
+            'admin/*',
+            'admin',
+            'livewire/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
