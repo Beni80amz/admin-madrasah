4e0c7fa4cbfdbe65e6ae58e7235b058e1832bc79
Beni80amz <benisolehudin@madrasah.kemenag.go.id>
2026-01-10 22:22:44 +0700

    Fix: Correct type hints for navigation properties in UserAccountResource

--- app/Filament/Resources/UserAccounts/UserAccountResource.php
+++ app/Filament/Resources/UserAccounts/UserAccountResource.php
@@ -5,16 +5,18 @@ namespace App\Filament\Resources\UserAccounts;
 use App\Filament\Resources\UserAccounts\Pages\ListUserAccounts;
 use App\Filament\Resources\UserAccounts\Tables\UserAccountsTable;
 use App\Models\User;
+use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class UserAccountResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-users';
 
-    protected static ?string $navigationGroup = 'Akademik';
+    protected static UnitEnum|string|null $navigationGroup = 'Akademik';
 
     protected static ?string $navigationLabel = 'Akun Pengguna';
 
