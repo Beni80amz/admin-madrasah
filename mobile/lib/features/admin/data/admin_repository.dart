9a246c4ad5b35872cc6c8778a70a697a6d398dc0
Beni80amz <benisolehudin@madrasah.kemenag.go.id>
2026-01-16 03:35:53 +0700

    Restore AdminRepository.dart content

--- mobile/lib/features/admin/data/admin_repository.dart
+++ mobile/lib/features/admin/data/admin_repository.dart
@@ -1,6 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../core/network/network_info.dart';
+import '../../../core/constants/api_constants.dart';
 import '../../auth/data/auth_repository.dart';
 
-// ...
+// Provider
+final adminRepositoryProvider = Provider<AdminRepository>((ref) {
+  final dio = ref.watch(dioProvider); 
+  return AdminRepository(dio, ref);
+});
+
+class AdminRepository {
+  final Dio _dio;
+  final Ref _ref;
+
+  AdminRepository(this._dio, this._ref);
 
   Options get _options {
     final token = _ref.read(authRepositoryProvider).getToken();
